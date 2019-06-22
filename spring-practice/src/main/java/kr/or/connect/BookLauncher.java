0da2d5a294fe947ff1782d5a57ba85e64a5bd6d1
woga1999 <woga1999@gmail.com>
2019-06-22 20:12:06 +0900

    Use DBCP
    dependency injection instead of spring dependecy injection

--- spring-practice/src/main/java/kr/or/connect/BookLauncher.java
+++ spring-practice/src/main/java/kr/or/connect/BookLauncher.java
@@ -1,18 +1,13 @@
 package kr.or.connect;
 
-import java.util.Collections;
-import java.util.Map;
-
-import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
-
 import kr.or.connect.bookserver.persistence.BookDao;
+import org.apache.commons.dbcp2.BasicDataSource;
 
 public class BookLauncher {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		BasicDataSource dataSource = new BasicDataSource();
 		dataSource.setDriverClassName("org.h2.Driver");
 		dataSource.setUrl("jdbc:h2:~/javaweb/db;AUTO_SERVER=TRUE;DB_CLOSE_ON_EXIT=FALSE;");
 		dataSource.setUsername("sa");
