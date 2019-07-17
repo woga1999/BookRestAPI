8309f2f214fc706a9cf5890f5aa8aa615fd9786e
woga1999 <woga1999@gmail.com>
2019-07-18 04:32:11 +0900

    수정 : 목록/상세 조회 REST API

--- book-app/book-server/src/main/java/kr/or/connect/bookserver/presentation/BookController.java
+++ book-app/book-server/src/main/java/kr/or/connect/bookserver/presentation/BookController.java
@@ -5,12 +5,14 @@ import java.util.Collection;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import kr.or.connect.bookserver.service.BookService;
 import kr.or.connect.domain.Book;
 
 @RestController
+@RequestMapping("/api/books")
 public class BookController {
 	private final BookService service;
 	
