0b80d64c70e10eb337cdca826293b16dc1d977e5
woga1999 <woga1999@gmail.com>
2019-07-18 03:18:55 +0900

    Add Controller

---
+++ book-app/book-server/src/main/java/kr/or/connect/bookserver/presentation/HelloController.java
@@ -0,0 +1,12 @@
+package kr.or.connect.bookserver.presentation;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	@GetMapping("/hello")
+	String hello() {
+		return "Hello World";
+	}
+}
